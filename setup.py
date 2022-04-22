147098ae4bd29dfdc7ba825fcde2a6cf9bdc1535
Olivier Hériveaux <olivier.heriveaux@ledger.fr>
2022-04-22 10:18:17 +0200

    Lint

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from setuptools import setup, find_packages
 import sys
 
 setup(
-    name='pystages',
-    version='1.0',
+    name="pystages",
+    version="1.0",
     # require pyserial
-    install_requires=['pyserial', 'pyusb', 'numpy'],
+    install_requires=["pyserial", "pyusb", "numpy"],
     packages=find_packages(),
-    python_requires='>=3.7')
+    python_requires=">=3.7",
+)
