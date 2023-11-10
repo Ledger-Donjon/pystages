359e0908d6f5b4143348cd1298c54886a304f003
mmouchous-ledger <michael.mouchous@ledger.fr>
2023-11-10 12:15:53 +0100

    Position label and regular updates (100ms)

--- pystages/gui/gui.py
+++ pystages/gui/gui.py
@@ -1,5 +1,13 @@
 #!/bin/python3
-from PyQt6.QtWidgets import QWidget, QHBoxLayout, QLabel, QComboBox, QPushButton
+from PyQt6.QtWidgets import (
+    QWidget,
+    QHBoxLayout,
+    QVBoxLayout,
+    QLabel,
+    QComboBox,
+    QPushButton,
+)
+from PyQt6.QtCore import QObject, QTimer
 from ..cncrouter import CNCRouter
 from ..corvus import Corvus
 from ..smc100 import SMC100
@@ -24,6 +32,11 @@ class StageWindow(QWidget):
                 self.stage = Corvus()
             elif selected == StageType.SMC:
                 self.stage = SMC100()
+            self.position_timer.start(100)
+        else:
+            del self.stage
+            self.stage = None
+            self.position_timer.stop()
 
     def __init__(self):
         super().__init__()
@@ -31,16 +44,31 @@ class StageWindow(QWidget):
         # Current stage
         self.stage: Stage = None
 
+        # This flag is used to limit the communication
+        # with the stage by not making updates of the position
+        self.in_motion = False
+
+        vbox = QVBoxLayout()
+        self.setLayout(vbox)
+
         box = QHBoxLayout()
+        vbox.addLayout(box)
         w = QLabel("Stage Selection")
         box.addWidget(w)
         self.stage_selection = w = QComboBox()
         w.addItems([StageType.CNC, StageType.Corvus, StageType.SMC])
-
         box.addWidget(w)
-        self.setLayout(box)
-
         self.connect_button = w = QPushButton("Connect")
         w.setCheckable(True)
         w.clicked.connect(self.connect)
         box.addWidget(w)
+        self.position_label = w = QLabel("Pos")
+        vbox.addWidget(w)
+        self.position_timer = QTimer()
+        self.position_timer.timeout.connect(self.update_position)
+
+    def update_position(self):
+        if self.stage is None or self.in_motion:
+            return
+        position = self.stage.position
+        self.position_label.setText(",".join([f"{i:.02f}" for i in position.data]))
