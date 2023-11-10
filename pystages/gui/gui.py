04fc424959fb1648c1c0c2c441548c577afdad3d
mmouchous-ledger <michael.mouchous@ledger.fr>
2023-11-10 12:19:31 +0100

    Move buttons

--- pystages/gui/gui.py
+++ pystages/gui/gui.py
@@ -3,9 +3,11 @@ from PyQt6.QtWidgets import (
     QWidget,
     QHBoxLayout,
     QVBoxLayout,
+    QGridLayout,
     QLabel,
     QComboBox,
     QPushButton,
+    QCheckBox,
 )
 from PyQt6.QtCore import QObject, QTimer
 from ..cncrouter import CNCRouter
@@ -62,6 +64,46 @@ class StageWindow(QWidget):
         w.setCheckable(True)
         w.clicked.connect(self.connect)
         box.addWidget(w)
+
+        box = QHBoxLayout()
+        vbox.addLayout(box)
+        box.addWidget(QLabel("Step (mm)"))
+        self.step_selection = w = QComboBox()
+        w.addItems(["10", "5", "1", "0.5", "0.1"])
+        w.setCurrentText("1")
+        box.addWidget(w)
+
+        grid = QGridLayout()
+        w = QPushButton("Y+")
+        w.clicked.connect(self.bouton_moved)
+        grid.addWidget(w, 0, 1)
+        w = QPushButton("X-")
+        w.clicked.connect(self.bouton_moved)
+        grid.addWidget(w, 1, 0)
+        w = QPushButton("Y-")
+        w.clicked.connect(self.bouton_moved)
+        grid.addWidget(w, 2, 1)
+        w = QPushButton("X+")
+        w.clicked.connect(self.bouton_moved)
+        grid.addWidget(w, 1, 2)
+        w = QPushButton("Z+")
+        w.clicked.connect(self.bouton_moved)
+        grid.addWidget(w, 0, 3)
+        w = QPushButton("Z-")
+        w.clicked.connect(self.bouton_moved)
+        grid.addWidget(w, 2, 3)
+        vbox.addLayout(grid)
+
+        box = QHBoxLayout()
+        vbox.addLayout(box)
+        self.z_offset = w = QCheckBox("Z offset (mm)")
+        w.setChecked(True)
+        box.addWidget(w)
+        self.z_offset_sel = w = QComboBox()
+        w.addItems(["10", "5", "1", "0.5", "0.1"])
+        w.setCurrentText("1")
+        box.addWidget(w)
+
         self.position_label = w = QLabel("Pos")
         vbox.addWidget(w)
         self.position_timer = QTimer()
@@ -72,3 +114,24 @@ class StageWindow(QWidget):
             return
         position = self.stage.position
         self.position_label.setText(",".join([f"{i:.02f}" for i in position.data]))
+
+    def bouton_moved(self):
+        if self.stage is None:
+            return
+        self.in_motion = True
+        button = QObject().sender()
+        axe, direction = button.text()
+        axe = {"X": 0, "Y": 1, "Z": 2}[axe]
+        step = float(direction + self.step_selection.currentText())
+        pos = self.stage.position
+        if axe != 2 and self.z_offset.isChecked():
+            pos[2] += float(self.z_offset_sel.currentText())
+            self.stage.move_to(pos)
+
+        pos[axe] += step
+        self.stage.move_to(pos)
+
+        if axe != 2 and self.z_offset.isChecked():
+            pos[2] -= float(self.z_offset_sel.currentText())
+            self.stage.move_to(pos)
+        self.in_motion = False
